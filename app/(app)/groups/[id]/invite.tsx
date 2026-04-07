0268bbd4fff5e7ecfd51335a749cc5e38f10a1a7
KillianHzr <killianherzer@gmail.com>
2026-04-07 14:42:51 +0200

    Update invitation link in share message to point to the new app landing page

--- app/(app)/groups/[id]/invite.tsx
+++ app/(app)/groups/[id]/invite.tsx
@@ -132,7 +132,7 @@ export default function InviteScreen() {
 
   const handleShareCode = async () => {
     try {
-      const message = `Rejoins mon cercle privé sur HappyOur !\n\nCode d'invitation : ${inviteCode}\n\nTélécharge l'app ici : https://happyour-landing.vercel.app`;
+      const message = `Rejoins mon cercle privé sur HappyOur !\n\nCode d'invitation : ${inviteCode}\n\nTélécharge l'app ici : https://app-gobelins-m2-landing.vercel.app/`;
       await Share.share({ message });
     } catch (e: any) {
       Alert.alert("Erreur", e.message);
